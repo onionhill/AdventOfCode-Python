8da7e6dcbdb6689f14dabfa6c24ebec822de0573
Vegard Løkås <vegardlokas@hotmail.com>
2023-12-11 22:22:25 +0100

    file

---
+++ 03/hello.py
@@ -0,0 +1,22 @@
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+        
+    try:
+        fn = float(num)
+    except:
+        print('Invalid input')
+        continue
+   
+    if largest is None or fn < largest:
+        largest = fn
+    if smallest is None or fn > smallest:
+        smallest = fn
+        
+    print(num)
+
+print("Maximum is", largest)
+print("Minimum is", smallest)
