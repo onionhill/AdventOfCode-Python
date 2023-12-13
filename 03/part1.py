47f3fac2fe0f28b53c40c7c08d668aaf40aacc32
Vegard Løkås <vegardlokas@hotmail.com>
2023-12-13 13:07:00 +0100

    done with day 3

--- 03/part1.py
+++ 03/part1.py
@@ -4,30 +4,60 @@ import re
 with open('input.txt') as sample :
     data = sample.readlines()
 
-
-def check_matrix(lineNumber, start,end) :
-    ## Check the line above
+def get_bounderies(line, start,end) :
     lineNumbers = []
-    if lineNumber > 0 : lineNumbers.append(lineNumber-1)
-    lineNumbers.append(lineNumber)
-    if lineNumber+1 < len(data) :  lineNumbers.append(lineNumber +1)
+    if line > 0 : lineNumbers.append(line-1)
+    lineNumbers.append(line)
+    if line+1 < len(data) :  lineNumbers.append(line +1)
 
     if start != 0:
         start = start -1
-    
-    for x in lineNumbers :
-        if re.search(r'[^.\d]', data[x][start:end] ):
-            return True
+    if end + 1 != len(data[0] ) :
+        end = end + 1
+    return {'lines': lineNumbers, 'start': start,'end':end}
 
+def check_matrix_for_special_char(bounderies) :
+    for x in bounderies['lines'] :
+        if re.search(r'[^.\d]', data[x][bounderies['start']:bounderies['end'] ]):
+            return True
     return False
 
-result = []
-for x in range(len(data ) ) :
-    line = data[x]
-    matches = re.finditer(r'\d+', line)
-    numbers_in_line = []
-    for match in matches :
-        if check_matrix(x, match.start(), match.end() ):        
-            result.append(int(match.group()))   
+def check_matrix_for_numbers(bounderies, numbers) :
+    result = []
+    for x in bounderies['lines'] :
+        if x in numbers:
+            for numb in numbers[x] :
+                if numb[1] >= bounderies['start'] and numb[1] <= bounderies['end'] :
+                    result.append(numb[0])
+                elif numb[2] >= bounderies['start'] and numb[2] <= bounderies['end'] :
+                    result.append(numb[0])
+    return result
 
-print('Part 1', sum(result))
+def part1() :
+    part1_result = []
+    for x in range(len(data ) ) :
+        line = data[x].rstrip()
+        matches = re.finditer(r'\d+', line)
+        for match in matches :
+            if not x in numbers_found : numbers_found[x] = []
+            numbers_found[x].append( [int(match.group() ),match.start(), match.end()-1] )
+            if check_matrix_for_special_char(get_bounderies(x, match.start(), match.end() )):       
+                part1_result.append(int(match.group()))   
+
+    print('Part 1', sum(part1_result))
+
+def part2() :
+    part2_result = []
+    for x in range(len(data ) ) :
+        line = data[x].rstrip()
+        matches = re.finditer(r'[^.\d]',line )
+        for match in matches :
+            part2_data = check_matrix_for_numbers(get_bounderies(x, match.start(), match.end()-1 ), numbers_found)
+            if len( part2_data) > 1:     
+                part2_result.append(part2_data[0] * part2_data[1] )
+
+    print('Part 1', sum(part2_result))
+
+numbers_found = dict()
+part1()
+part2()
